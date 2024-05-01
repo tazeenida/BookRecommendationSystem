9bfff336a2906f83f7aab11e87d5863cfaae543b
tazeenida <156009825+tazeenida@users.noreply.github.com>
2024-05-01 09:19:15 -0400

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-t-)ria0)x@55vk612e)3u0qi%&4fs_1063f@g+36mxbgjd7ol7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://bookrecommendationsystem-1.onrender.com']
+ALLOWED_HOSTS = ['bookrecommendationsystem-1.onrender.com']
 
 
 # Application definition
